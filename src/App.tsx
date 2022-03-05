00798c90b8affdf1d06e2d947b5d1cc9cf249732
Ashun <ashun.kothari10@gmail.com>
2022-03-05 10:33:59 +0000

    Adding style to the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback, useRef } from 'react';
-import produce from 'immer';
-import './App.css'
+import "bulma/css/bulma.min.css";
+import React, { useState, useCallback, useRef } from "react";
+import { Pause, Play, XCircle, Globe } from "react-feather";
+import produce from "immer";
+import "./App.css";
 
-const numRows = 50;
-const numCols = 50;
+const numRows = 25;
+const numCols = 35;
 
 const operations = [
   [0, 1],
@@ -13,41 +15,49 @@ const operations = [
   [1, 1],
   [-1, -1],
   [1, 0],
-  [-1, 0]
-]
+  [-1, 0],
+];
 
 const generateEmptyGrid = () => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++){
-      rows.push(Array.from(Array(numCols), () => 0));
-    } 
-    return rows;
-}
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
+
+const generateGridWithRandomCells = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0)));
+  }
+  return rows;
+};
 
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
     return generateEmptyGrid();
-  })
+  });
 
   const [running, setRunning] = useState(false);
 
   const runningRef = useRef(running);
-  runningRef.current = running
+  runningRef.current = running;
 
   const runSimulation = useCallback(() => {
-    if (!runningRef.current){
+    if (!runningRef.current) {
       return;
     }
     setGrid((g) => {
-      return produce(g, gridCopy => {
-        for (let i = 0; i < numRows; i++){
-          for (let k = 0; k < numCols; k++){
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
             let neighbours = 0;
             operations.forEach(([x, y]) => {
               const newI = i + x;
               const newK = k + y;
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols){
-                neighbours += g[newI][newK]
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbours += g[newI][newK];
               }
             });
 
@@ -60,58 +70,81 @@ const App: React.FC = () => {
         }
       });
     });
-    
+
     setTimeout(runSimulation, 100);
   }, []);
-  
-  return (
-    <>
-      <button onClick={()=>{
-      setRunning(!running);
-      if (!running) {
-        runningRef.current = true;
-        runSimulation();
-      }
-      
-      }} >{running ? "stop" : "start" }
-      </button>
-      <button onClick={()=> {
-        setGrid(generateEmptyGrid());
-      }}>clear
-      </button>
-      <button onClick={()=> {
-        setGrid(generateEmptyGrid());
-        const rows = [];
-        for (let i = 0; i < numRows; i++){
-          rows.push(Array.from(Array(numCols), () => Math.random() > 0.7 ? 1: 0));
-        }
 
-      setGrid(rows)
-      }}>random
-      </button>
-    <div style = {{
-      display: "grid",
-      gridTemplateColumns: `repeat(${numCols}, 20px)`
-    }}>
-      {grid.map((rows, i) => 
-      rows.map((col, k) => (
-        <div 
-          key={`${i} - ${k}`}
+  return (
+    <div className="container has-text-centered py-5">
+      <h2 className="title is-uppercase">Conway's Game of Life</h2>
+      <div className="buttons is-centered pt-5">
+        <button
+          className="button mx-2 is-success"
           onClick={() => {
-            const newGrid = produce(grid, gridCopy => {
-              gridCopy[i][k] = grid[i][k] ? 0 : 1;
-            })
-            setGrid(newGrid);
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
           }}
-          style = {{
-            width: 20, 
-            height: 20, backgroundColor:grid[i][k]? 'pink' : undefined, 
-            border: "solid 1px black"}}/>))
-               )}
+        >
+          <span className="icon">{running ? <Pause /> : <Play />}</span>
+          <span>{running ? "Stop" : "Start"}</span>
+        </button>
+        <button
+          className="button is-danger"
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          <span className="icon">
+            <XCircle />
+          </span>
+          <span>Clear</span>
+        </button>
+        <button
+          className="button mx-2 is-primary"
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+            setGrid(generateGridWithRandomCells());
+          }}
+        >
+          <span className="icon">
+            <Globe />
+          </span>
+          <span>Random</span>
+        </button>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          width: "fit-content",
+          margin: "0 auto",
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i} - ${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "#27ae60" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
     </div>
-    </>
-      );
-  
+  );
 };
 
 export default App;
